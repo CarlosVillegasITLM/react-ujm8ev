f291201a0f4198bac4f5066cf8803a484ec8a1c6
CarlosVillegasITLM <carlosvillegasitlm@gmail.com>
2019-06-25 18:42:14 -0600

    1 file modified

--- index.js
+++ index.js
@@ -3,13 +3,41 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
-function Componente(){
-  return <p>Hola Mundo</p>;
+
+/* function ComponenteA(props){
+  //console.log(props.children);
+  //return <p>Hola {props.nombre}</p>;
+  return props.children;
+}
+
+function ComponenteB(props){
+  return <p>{props.nombre}: 10</p>;
 }
 
 class ComponenteDeClase extends Component{
   render(){
     return <p>Hola soy de la clase</p>;
+  }
+} */
+
+class Contador extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      contador: 0
+    };
+  }
+
+  aumentar= ()=>{
+    this.setState(
+      {contador:this.state.contador + 1 }
+    )}
+
+  render(){
+    return (<div>
+      <p>{this.state.contador}</p>
+      <button onClick={ this.aumentar }>Aumentar</button>
+    </div>);
   }
 }
 
@@ -22,9 +50,10 @@ class App extends Component {
   }
 
   render() {
+    let nombre = "Carlos";
     return (
       <div>
-        < ComponenteDeClase />
+        < Contador />
       </div>
     );
   }
